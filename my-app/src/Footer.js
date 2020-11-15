f825f855a07d0bcd0c6de147e91a6dbc2147510c
peerpp <71480556+peerpp@users.noreply.github.com>
2020-11-15 21:36:42 +0800

    link github changed

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -5,9 +5,9 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer>
-      Last updated: Oct 11, 2020. Created by Vera Goossen :) <br />
+      Last updated: Nov 15, 2020. Created by Vera Goossen :) <br />
       The rights on the images on this page are reserved by the original owner.
-      <a href="https://github.com/peerpp/SheCodesWeatherAppVG" target="_blank">
+      <a href="https://github.com/peerpp/SheCodesWeatherAppReactVG" target="_blank">
         🎁Open-source Code🎁
       </a>
     </footer>
