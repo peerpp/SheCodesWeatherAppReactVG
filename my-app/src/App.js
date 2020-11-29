2a5d9004b766d55ebcbb8c0d4680a66e80b13aff
peerpp <71480556+peerpp@users.noreply.github.com>
2020-11-29 20:52:42 +0800

    Added front and back card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,27 +7,28 @@ import Footer from "./Footer";
 
 import axios from "axios";
 
+const apiKey = "71f57c13bbcc4d290991410e3cd840b3";
+
+let now = new Date();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 export default function App() {
   let [formInput, setFormInput] = useState("");
-
-  const apiKey = "71f57c13bbcc4d290991410e3cd840b3";
-
-  let now = new Date();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+  let [flipCards, setFlipCards] = useState({});
 
   function minutesTwoDigits(minutes) {
     if (minutes < 10) {
@@ -62,21 +63,98 @@ export default function App() {
     ];
   }
 
-  function displayCity(event) {
+  function formatShortDate(date) {
+    let day = days[date.getUTCDay()];
+    let month = months[date.getUTCMonth()];
+    let numDay = date.getUTCDate();
+    return `${day} ${month} ${numDay}`;
+  }
+
+  async function displayCity(event) {
+    event.preventDefault();
+
+    let inputLocation = event.target[0].value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&units=metric&appid=${apiKey}`;
+    let response1 = await axios
+      .get(apiUrl)
+      .catch((err) => setFormInput("Not found!"));
+
+    let latitude = response1.data.coord.lat;
+    let longitude = response1.data.coord.lon;
+    apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let response2 = await axios.get(apiUrl);
+
+    showTemperature(response1, response2);
+  }
+
+  /*
+  function displayCity2(event) {
     event.preventDefault();
     let inputLocation = event.target[0].value;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&units=metric&appid=${apiKey}`;
     axios
-      .get(`${apiUrl}&appid=${apiKey}`)
-      .then(showTemperature)
-      .catch((err) => setFormInput("Not found!"));
+      .get(apiUrl)
+      .then((response) => {
+        showTemperature(response);
+        return response;
+      })
+      .then((response) => getForecast3days(response))
+      .catch((err) => {
+        console.log(err);
+        setFormInput("Not found!");
+      });
   }
+*/
 
   function formInputChange(event) {
     setFormInput(event.target.value);
   }
 
-  function showTemperature() {}
+  function showTemperature(response1, response2) {
+    setFlipCards({
+      flipCard4: flipCards.flipCard3,
+      flipCard3: flipCards.flipCard2,
+      flipCard2: flipCards.flipCard1,
+      flipCard1: {
+        // front
+        temperature: Math.round(response1.data.main.temp),
+        description: response1.data.weather[0].description,
+        humidity: response1.data.main.humidity,
+        country: response1.data.sys.country,
+        wind: Math.round(response1.data.wind.speed),
+        time: new Date(
+          response1.data.dt * 1000 + response1.data.timezone * 1000
+        ),
+        feelsLike: Math.round(response1.data.main.feels_like),
+        inputLocation: formInput,
+        icon: response1.data.weather[0].icon,
+        flip: false,
+
+        // back
+        day1: formatShortDate(new Date(response2.data.daily[1].dt * 1000)),
+        day2: formatShortDate(new Date(response2.data.daily[2].dt * 1000)),
+        day3: formatShortDate(new Date(response2.data.daily[3].dt * 1000)),
+        temp1: Math.round(response2.data.daily[1].temp.day),
+        temp2: Math.round(response2.data.daily[2].temp.day),
+        temp3: Math.round(response2.data.daily[3].temp.day),
+        feels1: Math.round(response2.data.daily[1].feels_like.day),
+        feels2: Math.round(response2.data.daily[2].feels_like.day),
+        feels3: Math.round(response2.data.daily[3].feels_like.day),
+        icon1: response2.data.daily[1].weather[0].icon,
+        icon2: response2.data.daily[2].weather[0].icon,
+        icon3: response2.data.daily[3].weather[0].icon,
+        icon1alt: response2.data.daily[1].weather[0].description,
+        icon2alt: response2.data.daily[2].weather[0].description,
+        icon3alt: response2.data.daily[3].weather[0].description,
+      },
+    });
+  }
+
+  function flipCardClick(flipCardId) {
+    let copy = Object.assign({}, flipCards);
+    copy[flipCardId].flip = true;
+    setFlipCards(copy);
+  }
 
   return (
     <div>
@@ -86,25 +164,41 @@ export default function App() {
         formInputChange={formInputChange}
         onSubmit={displayCity}
       />
-      <br />
-      <div className="row">
-        <div className="col-6">
-          <Flipcard intro="Looking For A City-Trip! Tokyo? Paris? Shanghai?" />
-        </div>
-        <div className="col-6">
-          <Flipcard intro="Looking For A Beach Holiday! Bali? Hue?" />
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-6">
-          <Flipcard intro="Looking For A Cruise Trip! Sydney? Cuba? Penang?" />
-        </div>
-        <div className="col-6">
-          <Flipcard intro="Looking For A Road Trip! San Francisco? Auckland?" />
-        </div>
-      </div>
 
+      <br />
+      <div className="row">
+        <div className="col-6">
+          <Flipcard
+            intro="Looking For A City-Trip! Tokyo? Paris? Shanghai?"
+            data={flipCards.flipCard1}
+            onClick={() => flipCardClick("flipCard1")}
+          />
+        </div>
+        <div className="col-6">
+          <Flipcard
+            intro="Looking For A Beach Holiday! Bali? Hue?"
+            data={flipCards.flipCard2}
+            onClick={() => flipCardClick("flipCard2")}
+          />
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-6">
+          <Flipcard
+            intro="Looking For A Cruise Trip! Sydney? Cuba? Penang?"
+            data={flipCards.flipCard3}
+            onClick={() => flipCardClick("flipCard3")}
+          />
+        </div>
+        <div className="col-6">
+          <Flipcard
+            intro="Looking For A Road Trip! San Francisco? Auckland?"
+            data={flipCards.flipCard4}
+            onClick={() => flipCardClick("flipCard4")}
+          />
+        </div>
+      </div>
       <br />
 
       <Footer />
--- my-app/src/Flipcard.js
+++ my-app/src/Flipcard.js
@@ -1,50 +1,157 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./flipcard.css";
+import { getCountryName } from "./countrynames";
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 export default function Flipcard(props) {
+  let inputLocation = "Find your destination...";
+  let temperature;
+  let description = props.intro;
+  let humidity;
+  let icon = <i className="fas fa-search-location"></i>;
+  let wind;
+  let time = <span>&nbsp;</span>;
+  let feelsLike;
+  let buttonCardHidden = "btn btn-forecast btn-forecast-hidden buttonCard";
+  let flip = "flip-card";
+  let day1;
+  let day2;
+  let day3;
+  let temp1;
+  let temp2;
+  let temp3;
+  let feels1;
+  let feels2;
+  let feels3;
+  let icon1;
+  let icon2;
+  let icon3;
+
+  if (props.data) {
+    // front
+    let countryName = getCountryName(props.data.country);
+    inputLocation = `${props.data.inputLocation}, ${countryName}`;
+
+    let iconSrc = `https://openweathermap.org/img/wn/${props.data.icon}.png`;
+    icon = <img className="iconCardFront" src={iconSrc} alt={description} />;
+
+    temperature = <span>{props.data.temperature}°C &mdash;</span>;
+    description = props.data.description;
+    humidity = `Humidity: ${props.data.humidity}%`;
+    wind = `Wind: ${props.data.wind} km/h`;
+    time = formatUTCDate(props.data.time);
+    feelsLike = `Feels like: ${props.data.feelsLike}°C`;
+    buttonCardHidden = "btn btn-forecast buttonCard";
+
+    if (props.data.flip) flip = "flip-card flip";
+
+    // back
+    day1 = props.data.day1;
+    day2 = props.data.day2;
+    day3 = props.data.day3;
+    temp1 = props.data.temp1;
+    temp2 = props.data.temp2;
+    temp3 = props.data.temp3;
+    feels1 = props.data.feels1;
+    feels2 = props.data.feels2;
+    feels3 = props.data.feels3;
+    let icon1Src = `https://openweathermap.org/img/wn/${props.data.icon1}.png`;
+    icon1 = (
+      <img className="iconCardBack" src={icon1Src} alt={props.data.icon1alt} />
+    );
+    let icon2Src = `https://openweathermap.org/img/wn/${props.data.icon2}.png`;
+    icon2 = (
+      <img className="iconCardBack" src={icon2Src} alt={props.data.icon2alt} />
+    );
+    let icon3Src = `https://openweathermap.org/img/wn/${props.data.icon3}.png`;
+    icon3 = (
+      <img className="iconCardBack" src={icon3Src} alt={props.data.icon3alt} />
+    );
+  }
+
+  function minutesTwoDigits(minutes) {
+    if (minutes < 10) {
+      return `0${minutes}`;
+    } else {
+      return minutes;
+    }
+  }
+
+  function formatUTCDate(date) {
+    let day = days[date.getUTCDay()];
+    let hours = date.getUTCHours();
+    let minutes = minutesTwoDigits(date.getUTCMinutes());
+    let month = months[date.getUTCMonth()];
+    let numDay = date.getUTCDate();
+    let year = date.getUTCFullYear();
+    let ampm;
+    if (hours >= 12) {
+      ampm = "PM";
+    } else {
+      ampm = "AM";
+    }
+    if (hours > 12) {
+      hours = hours - 12;
+    } else if (hours === 0) {
+      hours = 12;
+    }
+    return [
+      `Last updated: ${day} ${month} ${numDay}, ${year} `,
+      <i className="far fa-clock" key="clock"></i>,
+      ` ${hours}:${minutes}${ampm}`,
+    ];
+  }
+
   return (
-    <div className="flip-card" id="flipCard1">
+    <div className={flip}>
       <div className="flip-card-inner">
         <div className="flip-card-front">
           <div className="card-resultsHistory">
             <div className="card-body">
-              <h5 className="card-title">
-                <span className="cityCountryCard">
-                  Find your destination...
-                </span>
-              </h5>
+              <h5 className="card-title">{inputLocation}</h5>
 
               <h6 className="card-subtitle-card">
                 <div className="row">
                   <div className="col-3">
-                    <span className="searchIcon">
-                      <i className="fas fa-search-location"></i>
-                    </span>
+                    {icon}
                     <br />
                   </div>
                   <div className="col-9">
-                    <span className="temperatureFront"></span>
-                    <span className="descriptionWeather descriptionCard">
-                      {props.intro}
-                    </span>
+                    {temperature}
+                    <span className="descriptionWeather"> {description}</span>
                     <br />
 
                     <span className="moreDetailsTempCard">
-                      <span className="feelsLikeCard"></span>
+                      {feelsLike}
                       <br />
-                      <span className="humidityCard"></span>
+                      {humidity}
                       <br />
-                      <span className="windCard"></span>
+                      {wind}
                     </span>
                   </div>
                 </div>
               </h6>
 
-              <button className="btn btn-forecast btn-forecast-hidden buttonCard">
+              <button className={buttonCardHidden} onClick={props.onClick}>
                 3 Days Forecast
               </button>
-              <div className="timeCard">&nbsp;</div>
+              <div className="timeCard">{time}</div>
             </div>
           </div>
         </div>
@@ -53,9 +160,7 @@ export default function Flipcard(props) {
           <div className="card card-forecastdays">
             <div className="card-body">
               <i className="far fa-window-close closeCard"></i>
-              <h5 className="card-title">
-                <span className="cityCountryCard"> </span>
-              </h5>
+              <h5 className="card-title">{inputLocation}</h5>
 
               <div className="container">
                 <div className="row">
@@ -65,44 +170,38 @@ export default function Flipcard(props) {
                 </div>
                 <br />
                 <div className="row">
-                  <div className="col-3 forecastCard day1"></div>
-                  <div className="col-3">
-                    <img src="" alt="" className="iconCardBack icon1" />
-                  </div>
+                  <div className="col-3 forecastCard day1">{day1}</div>
+                  <div className="col-3">{icon1}</div>
                   <div className="col-3 dayTemperature">
-                    <span className="temp1"></span>°C
+                    <span className="temp1">{temp1}</span>°C
                   </div>
                   <div className="col-3 feelsLikeCold">
-                    <span className="feels1"></span>°C
+                    <span className="feels1">{feels1}</span>°C
                   </div>
                 </div>
 
                 <div className="row">
-                  <div className="col-3 forecastCard day2"></div>
-                  <div className="col-3">
-                    <img src="" alt="" className="iconCardBack icon2" />
-                  </div>
+                  <div className="col-3 forecastCard day2">{day2}</div>
+                  <div className="col-3">{icon2}</div>
                   <div className="col-3 dayTemperature">
-                    <span className="temp2"></span>°C
+                    <span className="temp2">{temp2}</span>°C
                   </div>
                   <div className="col-3 feelsLikeCold">
-                    <span className="feels2"></span>°C
+                    <span className="feels2">{feels2}</span>°C
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-3 forecastCard day3"></div>
-                  <div className="col-3">
-                    <img src="" alt="" className="iconCardBack icon3" />
-                  </div>
+                  <div className="col-3 forecastCard day3">{day3}</div>
+                  <div className="col-3">{icon3} </div>
                   <div className="col-3 dayTemperature">
-                    <span className="temp3"></span>°C
+                    <span className="temp3">{temp3}</span>°C
                   </div>
                   <div className="col-3 feelsLikeCold">
-                    <span className="feels3"></span>°C
+                    <span className="feels3">{feels3}</span>°C
                   </div>
                 </div>
               </div>
-              <div className="timeCard timeCard1">&nbsp;</div>
+              <div className="timeCard">{time}</div>
             </div>
           </div>
         </div>
