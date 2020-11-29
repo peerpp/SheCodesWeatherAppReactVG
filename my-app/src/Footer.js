d2fa84749cb570634847057d58d9cb9dd741046d
peerpp <71480556+peerpp@users.noreply.github.com>
2020-11-29 21:26:58 +0800

    Updated footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -5,12 +5,15 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer>
-      Last updated: Nov 15, 2020. Created by Vera Goossen :) <br />
+      Last updated: Nov 29, 2020. Created by Vera Goossen :) <br />
       The rights on the images on this page are reserved by the original owner.
-      <a href="https://github.com/peerpp/SheCodesWeatherAppReactVG" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/peerpp/SheCodesWeatherAppReactVG"
+        target="_blank"
+        rel="noreferrer"
+      >
         🎁Open-source Code🎁
       </a>
     </footer>
   );
 }
-
